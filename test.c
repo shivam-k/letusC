4d22f07443ab35df96254755de1431476ecf9293
shivam-k <shivamkumar30013@gmail.com>
2016-12-20 01:07:46 +0530

    commit

--- test.c
+++ test.c
@@ -1,27 +1,33 @@
+#include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main()
 {
-	int *p, a=5;
-	int n[3][3] = {
-		2,4,3,
-		6,8,5,
-		3,5,1
-	};
+    int n, m, i, j, k, t, temp, count, num, pnum, arr[1000];
+    scanf("%d", &t);
+    for(i=0; i<t; i++)
+    {
+        scanf("%d", &n);
+        
+        num = 0;
+        for(x=n-1; x>=1; x--)
+        {
+            j=0;
+            count = x;
+            do {
+                arr[j] = count%10;
+                count = count/10;
+                j++;
+            } while(n>0);
+            for(k=j-1; k>=0; k--)
+            {   
+                num = num + arr[k]*temp;
+                temp = temp*10; 
+            }
+        }
+    }
 
-	int i, j;
-	for(i=0; i<=2; i++)
-		for(j=0; j<=2; j++)
-			printf("%d %d\n", n[i][j], *(*(n+i)+j));
-
-
-
-	//p = &n;
-	//printf("p = %p\n",p);
-	// int i, *ptr;
-	// ptr = n[0];
-	// for(i=0; i<=8; i++)
-	// 	printf("%d\n", *(ptr+i));
-	//printf("%p %p %d %d\n", *n, n[0], n[3][3], n[2][2]);
-
-	return 0;
+    return 0;
 }
