4ff7bcdbdd2da42bd846068669cc78fc9669e75a
shivam-k <shivamkumar30013@gmail.com>
2016-12-24 09:15:34 +0530

    24-12-2016

--- chap9/fh.c
+++ chap9/fh.c
@@ -6,21 +6,37 @@
 
 int main(void)
 {
-	char str[50], nstr[50];
-	char *rm = "the";
-	char *p;
+	char str[50], nstr[50], s[20][20];
+	char rm[] = "the";
+	int cmp;
 	scanf("%[^\n]s", str);
-	int i, j;
+	printf("%s\n", str);
+	int i = 0, j=0, k=0;
 	int size = strlen(str);
-	p = strtok (str," ,.-");
-	while(p != NULL)
+	for(i=0; i<size; i++)
 	{
-		if(p != rm)
-			printf("%s ", p);
-    	p = strtok (NULL, " ,.-");
+		if(str[i]==' ')
+		{
+			s[j][k] = '\0';
+			j++;
+			k=0;
+		}
+		else
+		{
+			s[j][k] = str[i];
+			k++;
+		}
 	}
-
+	s[j][k] = '\0';
 	printf("\n");
-
+	for(i=0; i<=j; i++)
+	{
+		cmp = strcmp(s[i], rm);
+		if(cmp!=0)
+		{
+			printf("%s ", s[i]);
+		}
+	}
+ 	printf("\n");
 	return 0;
 }
