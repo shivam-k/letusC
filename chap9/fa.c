1d7b9e75af41d91f4e462f213f3d77ef8b4168e3
shivam-k <shivamkumar30013@gmail.com>
2016-12-26 17:35:15 +0530

    26-12-2016

--- chap9/fa.c
+++ chap9/fa.c
@@ -12,37 +12,66 @@ int main(void)
 		"..all through C!"
 	};
 	char str1[20], str2[20], temp[20][20], *cpy;
-	int i, n, j, k=0, m=0, len1, len2;
-	//scanf("%s %s",str1, str2);
-	//printf("%d\n", 
-	//len1 = strlen(str[0]);
-	//printf("len1 = %d", len1);
+	scanf("%s %s", str1, str2);
+	int i, n, j, k=0, m=0, len1=strlen(str1), len2=strlen(str2), pass=0, row[10], col[10], r=0, c=0, tar;
 	for(i=0; i<6; i++)
 	{
-		m = 0;
-		for(j=0; str[i][j]!='\0'; j++)
+		j=0;
+		while(str[i][j]!='\0')
 		{
-			if(str[i][j]==' ' || str[i][j]=='.')
+			if(str[i][j]==str1[0])
 			{
-				temp[k][m] = '\0';
-				k++;
-				m=0;
+				k=0;
+				m=j;
+				while(str1[k]!='\0' || str[i][m]!='\0' || str[i][m]!=' ')
+				{
+					if(str[i][m]==str1[k])
+					{
+						if(k==len1-1)
+						{
+							row[r]=i;
+							col[c]=j;
+							r++; c++;
+						}
+						k++;
+						m++;
+					}
+					else 
+						break;
+				}
 			}
-			else
-			{
-				temp[k][m] = str[i][j];
-				m++;
-			}
+			j++;
 		}
-		printf("m = %d\n", m);
-		temp[k][m] = '\0';
-		printf("\n*****\n");
-		for(int n=0; n<k; n++)
-		{
-			printf("%s\n", temp[n]);
-		}
-		k++;
 	}
+	j=0;
+	for(i=0; i<r; i++)
+	{
+		tar = col[i]+len1;
+		for(j=col[i]; j<tar;j++)
+		{
+			printf("%c", str[row[i]][j]);
+		}
+		tar = col[i]+len2;
+		m = 0;
+		for(j=col[i]; j<tar; j++)
+		{
+			str[row[i]][j] = str2[m];
+			m++; 	
+			if(j==tar-1)
+			{
+				j++;
+				str[row[i]][j] = '\0';
+			}
+		}
+		
+		printf("\n");
+	}
+	for(i=0; i<6; i++)
+	{
+		printf("%s\n", str[i]);
+	}
+
+
 	printf("\n");
 	return 0;
 }
