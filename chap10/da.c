08ac7beac7396a568bb035722d0876d352ff3341
shivam-k <shivamkumar30013@gmail.com>
2016-12-28 19:05:43 +0530

    28-12-2016

--- chap10/da.c
+++ chap10/da.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string.h>
 #include <limits.h>
+//Structure declaration
 struct students
 {
 	int roll;
@@ -12,7 +13,11 @@ struct students
 	char cors[20];
 	int yoj;
 };
+// declaration of function which print names of all students who joined in a particular year
 void pyear(struct students *, int);
+// declaration of function which print data of students
+void details(struct students *, int);
+// main function
 int main(void)
 {
 	struct students s[450];
@@ -26,16 +31,17 @@ int main(void)
 		scanf("%s", s[i].cors);
 		scanf("%d", &s[i].yoj);
 	}
-	for(i=0; i<n; i++)
-	{
-		printf("%d\n", s[i].roll);
-		printf("%s\n", s[i].name);
-		printf("%s\n", s[i].dep);
-		printf("%s\n", s[i].cors);
-		printf("%d\n", s[i].yoj);	
-	}
-	pyear(&s[0], n);
-
+	// for(i=0; i<n; i++)
+	// {
+	// 	printf("%d\n", s[i].roll);
+	// 	printf("%s\n", s[i].name);
+	// 	printf("%s\n", s[i].dep);
+	// 	printf("%s\n", s[i].cors);
+	// 	printf("%d\n", s[i].yoj);	
+	// }
+	pyear(s, n);
+	details(s, n);
+	printf("\n");
 	return 0;
 }
 
@@ -43,9 +49,24 @@ void pyear(struct students *p, int n)
 {
 	int pyr, i;
 	scanf("%d", &pyr);
-	for(i=0; i<2; i++)
+	for(i=0; i<n; i++)
 	{
-		if(p[i]->yoj==pyr)
-			printf("%s\n", p[i]->name);
+		if(p[i].yoj==pyr)
+			printf("%s\n", p[i].name);
+	}
+}
+void details(struct students *p, int n)
+{
+	int i, roll_no;
+	scanf("%d", &roll_no);
+	for(i=0; i<n; i++)
+	{
+		if(p[i].roll==roll_no)
+		{
+			printf("%s\n", p[i].name);
+			printf("%s\n", p[i].dep);
+			printf("%s\n", p[i].cors);
+			printf("%d\n", p[i].yoj);
+		}
 	}
 }
