ea1245e9a8d642331fecdb8884fa4bad6cea983e
shivam-k <shivamkumar30013@gmail.com>
2017-01-04 09:25:54 +0530

    4-1-2017

--- test.c
+++ test.c
@@ -1,77 +1,91 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-// void swap(int **, int **);
-// int main(void)
-// {
-// 	//int *p, *q, *temp;
-// 	int i, j, min, t;
-// 	int n1 = 5, n2 = 10;
-// 	int *p1 = &n1, *p2 = &n2;
-// 	int **q1 = &p1, **q2 = &p2;
-// 	printf("n1 = %d, n2 = %d\n", n1, n2);
-// 	printf("&n1 = %p, &n2 = %p\n", &n1, &n2);
-// 	printf("p1 = %p, p2 = %p\n", p1, p2);
-// 	printf("*p1 = %d, *p2 = %d\n", *p1, *p2);
-// 	printf("&p1 = %p, &p2 = %p\n", &p1, &p2);
-// 	printf("q1 = %p, q2 = %p\n", q1, q2);
-// 	printf("*q1 = %p, *q2 = %p\n", *q1, *q2);
-// 	printf("**q1 = %d, **q2 = %d\n", **q1, **q2);
-// 	swap(q1, q2);
-// 	printf("***********");
-// 	printf("n1 = %d, n2 = %d\n", n1, n2);
-// 	printf("&n1 = %p, &n2 = %p\n", &n1, &n2);
-// 	printf("p1 = %p, p2 = %p\n", p1, p2);
-// 	printf("*p1 = %d, *p2 = %d\n", *p1, *p2);
-// 	printf("&p1 = %p, &p2 = %p\n", &p1, &p2);
-// 	printf("q1 = %p, q2 = %p\n", q1, q2);
-// 	printf("*q1 = %p, *q2 = %p\n", *q1, *q2);
-// 	printf("**q1 = %d, **q2 = %d\n", **q1, **q2);
-// 	// scanf("%d", &t);
-// 	// for(i=0; i<t; i++)
-// 	// {
-// 	// 	scanf("%d", &*p);
-// 	// 	if(i==0)
-// 	// 		q=p;
-// 	// 	p++;
-// 	// }
-// 	// min = p[0];
-// 	// // for(i=0; i<t; i++)
-// 	// // {
-// 	// // 	min = p[0];
-// 	// // 	for(j=i+1; j<t; j++)
-// 	// // 	{
-// 	// // 		if(p[j]<min)
-// 	// // 		{
-// 	// // 			temp[0] = p[j];
-// 	// // 			p[j] = p[i];
-// 	// // 			p[i] = temp[0];
-// 	// // 		}
-
-// 	// // 	}
-// 	// // }
-// 	// for(i=0; i<t; i++)
-// 	// {
-// 	// 	printf("%d\n", *q);
-// 	// 	q++;
-// 	// }
-
-// 	return 0;
-// }
-// void swap(int ∗∗a , int ∗∗b ) {
-// 	int ∗temp = ∗a ;
-// 	∗a = ∗b ;
-// 	∗b = temp ;
-// }
-
+#include <string.h>
+ 
 int main()
-
 {
+    int t, n, i, j, pass=0, con, count1=0, count2=0, chk1, chk2, k=0, m=0, blarr[26], slarr[26], flow, len;
+    char s[500];
+    for(i=65; i<=90; i++)
+    {
+        for(j=2; j<i; j++)
+        {
+            if(i%j!=0)
+                flow=1;
+            else
+            {
+                flow=0;
+                break;
+            }
+        }
+        if(flow==1)
+        {
+            blarr[k] = i;
+            k++;
+        }
+    }
+    m = 0;
+    for(i=97; i<=122; i++)
+    {
+        for(j=2; j<i; j++)
+        {
+            if(i%j!=0)
+                flow=1;
+            else
+            {
+                flow=0;
+                break;
+            }
+        }
+        if(flow==1)
+        {
+            slarr[m] = i;
+            m++;
+        }
+    }
+    for(i=0; i<k; i++)
+        printf("%d %c * ", blarr[i], blarr[i]);
+    printf("\n");
+    for(i=0; i<m; i++)
+        printf("%d %c * ", slarr[i], slarr[i]);
+    printf("\n");
+    scanf("%d", &t);
+    for(con=0; con<t; con++)
+    {
+        scanf("%d", &n);
+        scanf("%s", s);
+        len = strlen(s);
+        for(i=0; i<len; i++)
+        {
+            if(s[i]>=65 && s[i]<=90)
+            {
+                for(j=0; j<k; j++) //loop refers to array containing odd number ascii values beetween 65 & 90
+                {
+                    if(blarr[j]>s[i])
+                    {
+                        count1++;
+                        chk1 = blarr[j] - s[i];
+                        chk2 = s[i] - blarr[j-1];
+                        if(chk1<chk2)
+                            printf("%c", blarr[j]);
+                        else if(chk1>chk2)
+                            printf("%c", blarr[j-1]);
+                        else if(chk1==chk2)
+                            printf("%c", blarr[j-1]);
+                        break;
+                    }
+                    if(j==k-1 && count1==0)
+                        printf("%c", blarr[j]);
+                }
+            }
+            else if(s[i]>=97 && s[i]<=122)  //loop refers to array containing odd number ascii values beetween 65 & 90
+            {
 
-int arr[] = {10, 20, 30, 40, 50, 60};
-int *p=arr;
-*p++;
-printf("%d\n",*p);
-++(*p);
-printf("%d\n",*p);
+            }
+        }
+    }
+ 
+    printf("\n");
+ 
+    return 0;
 }
