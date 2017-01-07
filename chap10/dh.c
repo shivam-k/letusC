31ec9ab0b9600a28a25288a928dda990e7273567
shivam-k <shivamkumar30013@gmail.com>
2017-01-07 09:17:18 +0530

    7-1-2017

---
+++ chap10/dh.c
@@ -0,0 +1,23 @@
+/*---- I can do it ----*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <limits.h>
+struct node
+{
+	int val;
+	struct node *address;
+};
+void display(struct node n)
+{
+
+}
+
+int main(void)
+{
+	struct head, mid, last;
+	scanf("s")
+
+	return 0;
+}
