692ea386223d09aa25089540fa5f75aea7019a4b
shivam-k <shivamkumar30013@gmail.com>
2017-01-07 09:16:03 +0530

    7-1-2017

--- chap10/test.c
+++ chap10/test.c
@@ -4,32 +4,17 @@
 #include <math.h>
 #include <string.h>
 #include <limits.h>
-// struct book
-// {
-// 	char name[25];
-// 	char author[25];
-// 	int callno;
-// };
-// void display(struct book (*b)[2], int);
-int main(void)
+
+int main()
 {
-  struct student 
-  {
-    char name[20];
-    int roll;
-  };
-  struct student s1 = { "adam", 101 };
-  struct student s2 = s1;
-  printf("%s",s2.name);
+	//pointer to an array of 4 ints 
+	int (*ptr)[4] = {25,56,89,41}; // ==int *ptr  
+
+	//arr of 10 int* pointers
+	//int *p[4] = {125, 545, 584, 545}; 
+
+	printf("%d\n", (*ptr)[2]);
 
 	return 0;
 }
 
-// void display(struct book (*b)[2], int n)
-// {
-// 	int i;
-// 	for(i=0; i<n; i++)
-// 	{
-// 		printf("%s\n%s\n%d\n", b[i]->name, b[i]->author, b[i]->callno);
-// 	}
-// }
